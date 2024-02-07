10b96494e2a7592f1a5e59e4b217d3050fc3b295
Bhavya Shah <bhavya.shah1904@gmail.com>
2024-02-07 15:17:06 +0530

    events and parameters

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import {useState} from 'react'
  function UserInfo({name, inputAge}) {
   // const {name, age} = props
-  const [age, incrAge] = useState(inputAge)
+  const [age, setAge] = useState(inputAge)
 
+  const updateAge = (input) => {
+    input === '+' ? setAge(age + 1) : setAge(age-1)
+  }
   
   return (
     <div>
     <div>Name : {name}</div>
     <div>Age : {age}</div>
-    <button onClick={() => incrAge(age + 1)}>Increase Age</button>
-    <button onClick={() => incrAge(age - 1)}>Decrease Age</button>
+    <button onClick={() => updateAge('+')}>Increase Age</button>
+    <button onClick={() => updateAge('-')}>Decrease Age</button>
     </div>
   )
 }
